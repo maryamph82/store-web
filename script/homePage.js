5e727030dbae87de04f4965c436ff6091b0b2002
Maryam poorhasan <maryam82prh@gmail.com>
2025-01-09 01:22:28 +0330

    add search box , add footer about in home page , changes in show product

---
+++ script/homePage.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('input')
+let search = ''
+
+input.addEventListener('keydown', searchFunc)
+
+function searchFunc(event) {
+    if (event.key == "Enter") {
+        console.log(event.key)
+        console.log("search " + search)
+    } else {
+        search = search + event.key
+    }
+}
