c9ec19e470d46e21a16739cd77ce4f308924027d
Maryam poorhasan <maryam82prh@gmail.com>
2025-02-06 06:09:31 +0330

    Design a page to display search results and add a question and answer about why this store is on the contact us page.

---
+++ script/contact.js
@@ -0,0 +1,14 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("answer1").classList.add("visible");
+    }, 1000); 
+
+    setTimeout(function() {
+        document.getElementById("answer2").classList.add("visible");
+    }, 2000); 
+
+    setTimeout(function() {
+        document.getElementById("answer3").classList.add("visible");
+    }, 3000); 
+});
